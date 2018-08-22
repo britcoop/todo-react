77a55c06088a0bd14eb1a0a1a5910cfe3150909d
Brittany Cooper <b.cooperbc@gmail.com>
2018-08-22 07:12:14 -0400

    made todos editable

--- todo/src/components/AddItem.js
+++ todo/src/components/AddItem.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import EditableLabel from 'react-inline-editing';
 
 import './components.css'
 
 const TodoItem = (props) => {
   const { todo, index } = props;
+
   return (
-    <li className={todo.done? 'list-group-item complete' : 'list-group-item'}>
-      <span className={todo.done ? 'done' : ''}> <p> {todo.title}</p></span>
-      <span className={todo.done ? 'done' : ''}> <p>{todo.description}</p> </span>
-      <span className={todo.done ? 'done' : ''}> <p>Due: {todo.dueDate}</p> </span>
+    <li className={todo.done? 'list-group-item done' : 'list-group-item'}>
+      <EditableLabel inputClassName={todo.done ? 'done' : ''} text={todo.title ? `${todo.title}` : 'Add a title for this task'} />
+      <EditableLabel inputClassName={todo.done ? 'done' : ''} text={todo.description ? `${todo.description}` : 'Add a description for this task'} />
+      <EditableLabel inputClassName={todo.done ? 'done' : ''} text={todo.dueDate ? `Due: ${todo.dueDate}` : 'Due: DD/MM/YYYY' }/>
       <div className="form-check-inline">
         <input className="form-check-input" onChange={(event) => props.toggleTodoDone(event, index)} type="checkbox" checked={todo.done}/>
-        <label className="form-check-label">Complete</label>
+        <label className="form-check-label">Status: {todo.done ? "done" : "pending"}</label> 
       </div>
       <div className="btn-group float-right">
-        <button className="btn btn-link" onClick={() => props.editTodo(index)}>Edit</button>
         <button className="btn btn-link" onClick={() => props.deleteTodo(index)}>Delete</button>
       </div>
     </li>
--- todo/src/components/ToDoForm.js
+++ todo/src/components/ToDoForm.js
@@ -10,15 +10,16 @@ const TodoForm = (props) => {
       <form onSubmit={props.handleFormSubmit}>
         <div className="form-group">
           <label>Add a new todo!</label>
-          <input type="text" ref={input => this.todoTitle = input} />className="form-control" onChange={props.handleNewTodo} placeholder="add a new todo..."/>
+          <input type="text" className="form-control" onChange={props.handleNewTodo} placeholder="add a new todo..."/>
         </div>
         <div className="form-group">
           <textarea type="text" className="form-control" onChange={props.handleNewTodoDescription} placeholder="description"/>
         </div>
         <div className="form-group">
           <DatePicker
+            className="form-control"
             placeholderText="Select due date"
-            selected={moment(props.newTodoDate.value)} //this won't update! selected value must be an object but that returns a different error
+            // selected={moment(props.todo.dueDate)} //this doesn't update. selected value must be an object but that returns a different error
             onChange={props.handleNewTodoDate}
           />
         </div>
